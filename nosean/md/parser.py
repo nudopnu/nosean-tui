560bb68eb181ff6d8a311eb9e5ea29235a05c3a7
nudopnu <forsuse@live.de>
2025-12-18 23:59:55 +0100

    Added basic knowledge parsing

--- nosean/md/parser.py
+++ nosean/md/parser.py
@@ -1,15 +1,25 @@
 import json
+from functools import cache
+from typing import TypedDict
 
 from .tokenizer import Tokenizer, TokenDict, ContainerTokenDict
 
+class TokenNodeDict(TypedDict):
+    token: str
+    children: list["TokenNodeDict"]
+
+class KnowledgeItemDict(TypedDict):
+    title: str
+    content: str
 
 class Parser:
 
     def __init__(self, tokens: list[TokenDict]):
         self.tokens = tokens
     
+    @cache
     def ast(self):
-        root = {"token": "root", "children": []}
+        root: TokenNodeDict = {"token": "root", "children": []}
         parents = []
         tmp: list = root["children"]
         for tok in tokens:
@@ -27,7 +37,43 @@ class Parser:
             del tok["start"]
             tmp = tok["children"]
         return root
+    
+    def knowledge_items(self):
+        root = self.ast()
+        path: list[str] = []
+        result: dict[str, KnowledgeItemDict] = {}
+        
+        def is_node_tag(node: TokenNodeDict, tag: str):
+            return node["token"] == "html" and node["tagname"] == tag
+        
+        def get_raw(node: TokenNodeDict):
+            raw: str = node["raw"]
+            if "children" not in node:
+                return raw
+            for child in node["children"]:
+                raw += get_raw(child)
+            return raw
 
+        def traverse(token: TokenNodeDict):
+            nonlocal path
+            for child in token["children"]:
+                if child["token"] == "heading":
+                    level = child["level"]
+                    title = child["title"]
+                    path = ["" if idx >= len(path) else path[idx] for idx in range(level + 1)]
+                    path[level] = title
+                    print(path)
+                elif is_node_tag(child, "details"):
+                    content = ""
+                    title = "?"
+                    for sub_child in child["children"]:
+                        if is_node_tag(sub_child, "summary"):
+                            title = "".join(get_raw(x) for x in sub_child["children"]).strip()
+                        else:
+                            content += get_raw(sub_child).strip()
+                    result[title] = content
+        traverse(root)
+        return result
 
 if __name__ == "__main__":
     sample = """
@@ -45,4 +91,5 @@ abc
     # sample = Path("data/bash.md").read_text(encoding="utf8")
     tokens = Tokenizer().tok(sample)
     p = Parser(tokens)
-    print(json.dumps(p.ast()))
+    # print(json.dumps(p.ast()))
+    print(p.knowledge_items())
